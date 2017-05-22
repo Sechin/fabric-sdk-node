9168c9135d4e75681e6081bedbba76f527135e12
Oleksandr Sechyn <olexandr.sechyn@itl-g.com>
2017-05-21 23:39:16 -0700

    First

--- test/unit/util.js
+++ test/unit/util.js
@@ -33,7 +33,7 @@ module.exports.CHAINCODE_MARBLES_PATH = 'github.com/marbles_cc';
 module.exports.END2END = {
 	channel: 'mychannel',
 	chaincodeId: 'end2end',
-	chaincodeVersion: 'v1'
+	chaincodeVersion: 'v2'
 };
 
 // directory for file based KeyValueStore
